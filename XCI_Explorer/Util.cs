8101cf961fb06ef22b50234e34b289f0bbc63e10
StudentBlake <6874208+StudentBlake@users.noreply.github.com>
2018-07-18 11:24:45 -0400

    Reverted hash support because some games (Sonic Mania) cause the program to break

--- XCI_Explorer/Util.cs
+++ XCI_Explorer/Util.cs
@@ -107,5 +107,15 @@ namespace XCI_Explorer {
                     where x % 2 == 0
                     select Convert.ToByte(hex.Substring(x, 2), 16)).ToArray();
         }
+
+        public static string Base64Encode(string plainText) {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            return System.Convert.ToBase64String(plainTextBytes);
+        }
+
+        public static string Base64Decode(string base64EncodedData) {
+            var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
+            return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
+        }
     }
 }
