5b8d8e3b40a9b29fb75c9681c8730e5838c2bc11
Franz Plam <franzl@live.at>
2018-07-21 23:47:16 +0200

    Reworked Basic UnitTest Project

---
+++ XCI.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("XCI.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("XCI.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0dcf6fa0-6022-4dc8-803b-d8afe4c9fe61")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ XCI.UnitTests/UtilTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+using XCI.Explorer.Helpers;
+
+namespace XCI.UnitTests
+{
+    [TestFixture]
+    public class UtilTests
+    {
+        [TestCase(248, "2GB")]
+        [TestCase(240, "4GB")]
+        [TestCase(224, "8GB")]
+        [TestCase(225, "16GB")]
+        [TestCase(226, "32GB")]
+        public void GetCapacity_ValidCapacity(int givenId, string expectedCapacity)
+        {
+            var result = Util.GetCapacity(givenId);
+            result.Should().Be(expectedCapacity);
+        }
+
+        [Test]
+        public void GetCapacity_InvalidCapacity()
+        {
+            var result = Util.GetCapacity(0);
+            result.Should().Be("Unrecognized Size");
+        }
+
+        [TestCase(0, "MasterKey0 (1.0.0-2.3.0)")]
+        [TestCase(1, "MasterKey0 (1.0.0-2.3.0)")]
+        [TestCase(2, "MasterKey1 (3.0.0)")]
+        [TestCase(3, "MasterKey2 (3.0.1-3.0.2)")]
+        [TestCase(4, "MasterKey3 (4.0.0-4.1.0)")]
+        [TestCase(5, "MasterKey4 (5.0.0+)")]
+        [TestCase(6, "MasterKey unknown")]
+        public void GetMasterKey_MasterKeyIsKnown(byte givenId, string expectedMasterKey)
+        {
+            var result = Util.GetMasterKey(givenId);
+            result.Should().Be(expectedMasterKey);
+        }
+
+        [Test]
+        public void GetMasterKey_MasterKeyIsUnknown()
+        {
+            var result = Util.GetMasterKey(6);
+            result.Should().Be("MasterKey unknown");
+        }
+
+        [Test]
+        public void StringToByteArray_ValidConversion()
+        {
+            var stringToConvert = ("536f6d65537472696e67").ToUpper();
+            var result = Util.HexStringToByteArray(stringToConvert);
+            var expectedResult = BitConverter.ToString(result).Replace("-", "");
+
+            expectedResult.Should().Be(stringToConvert);
+        }
+    }
+}
